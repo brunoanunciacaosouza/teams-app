fc2431bff07071c154b9381cf71c9e281abd96e6
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-03 11:15:28 -0300

    criando tema

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 
+import { ThemeProvider } from "styled-components";
+
 import { StatusBar } from "react-native";
 
 import Groups from "@screens/Groups";
 
+import theme from "@theme/index";
+
 export default function App() {
   return (
     <>
-      <Groups />
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <ThemeProvider theme={theme}>
+        <Groups />
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent
+        />
+      </ThemeProvider>
     </>
   );
 }
