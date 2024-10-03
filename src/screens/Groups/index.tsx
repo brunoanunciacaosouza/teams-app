cc6749e3fb4a09aeb4afedcd8a8a0c769c8340bf
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-03 13:10:34 -0300

    listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,15 +3,22 @@ import Highlight from "@components/Highlight";
 
 import { Container } from "./styles";
 import GroupCard from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da faculdade", "Amigos"]);
   return (
     <Container>
-      <Header showBackButton={true} />
+      <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
