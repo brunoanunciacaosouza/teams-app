fde3dde26f0c71317244aa03e81ed0e05660207c
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-03 10:48:45 -0300

    estruturando projeto

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import { StatusBar } from "react-native";
 
-import Groups from "./src/Groups";
+import Groups from "@screens/Groups";
 
 export default function App() {
   return (
     <>
       <Groups />
       <StatusBar
-        barStyle="light-content"
+        barStyle="dark-content"
         backgroundColor="transparent"
         translucent
       />
