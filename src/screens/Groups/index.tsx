2e68598d7f9c781f0ad786602ebd1b8dec792d2e
Bruno Souza <brunoanunciacaosouza@gmail.com>
2024-10-03 12:59:52 -0300

    componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,12 +2,16 @@ import Header from "@components/Header";
 import Highlight from "@components/Highlight";
 
 import { Container } from "./styles";
+import GroupCard from "@components/GroupCard";
 
 export default function Groups() {
   return (
     <Container>
       <Header showBackButton={true} />
+
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
